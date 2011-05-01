0d31c4b4f95df04fb1589c938b3b9a3d133fd1e5
Walter Zheng <zdwalter@gmail.com>
2011-05-01 10:19:35 +0800

    add licence

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+/*
+ 
+Copyright (c) 2011 Walter Zheng (zdwalter@gmail.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Based on the retwis (0.3) PHP [example] application provided by Redis at redis.io
+Based on the [retwis-pytor](https://github.com/ajzeneski/retwis-pytor)
+This is complete re-write in Javascript using the Node.js and Express framework.
+
+Usage
+----
+
+    >>> node ./app.js
+
+Dependencies
+------------
+    * Redis (redis.io)
+    * Node (nodejs.org)
+    * Express (expressjs.com)
+    * node_redis (https://github.com/mranney/node_redis)
+
+  
+ */
 
 /**
  * Module dependencies.
